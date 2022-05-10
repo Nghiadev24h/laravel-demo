ccb7a2c3855c00163b27a231a5da3c3155d691a6
macos <macos@macoss-iMac-Pro.local>
2022-05-10 15:38:28 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\customer::factory(50)->create();
     }
 }
